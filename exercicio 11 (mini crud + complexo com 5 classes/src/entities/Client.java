4362b593b4617acda202fac563f31581609de0ee
Victor Americo <victoroeste12@gmail.com>
2025-08-29 11:17:09 -0300

    pensei em por esse exercicio separado, porqyue é praticamente um mini projeto, mas como nao commitei nada e nao registrei nada por nao saber usar o git decidi manter ele aqui mesmo, o proximo que eu fizer, eu faço mais elaborado com o git e com commits documentando tudo que eu fiz no projeto.

---
+++ exercicio 11 (mini crud + complexo com 5 classes/src/entities/Client.java
@@ -0,0 +1,57 @@
+package entities;
+
+import java.util.Date;
+
+public class Client {
+    private String name;
+    private String email;
+    private Date birthDate;
+
+    public Client(){
+
+    }
+    //construtor
+    public Client(String name, String email, Date birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+    //getters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    //seters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
---
+++ exercicio 11 (mini crud + complexo com 5 classes/src/entities/Order.java
@@ -0,0 +1,89 @@
+package entities;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+    private Date moment;
+    private OrderStatus status;
+    private Client client;
+    private List<OrdemItem> items;
+
+    //construtores
+
+
+    public Order(Date moment, List<OrdemItem> items, Client client, OrderStatus status) {
+        this.moment = moment;
+        this.items = new ArrayList<>();
+        this.client = client;
+        this.status = status;
+    }
+    //getters
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public List<OrdemItem> getItems() {
+        return items;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    //setters
+
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public void setItems(List<OrdemItem> items) {
+        this.items = items;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    //metodos
+
+    public void addItem(OrdemItem item) {
+        this.items.add(item);
+        System.out.println("Item" + item + "adicionado");
+    }
+    public void removeItem(OrdemItem item) {
+        this.items.remove(item);
+        System.out.println("Item" + item + " removido");
+    }
+    public Double total() {
+        Double soma = 0.0;
+        for (Integer i = 0; i < items.size(); i++){
+            soma += items.get(i).subTotal();
+        }
+        return soma;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "moment=" + moment +
+                ", status=" + status +
+                ", client=" + client +
+                ", items=" + items +
+                '}';
+    }
+}
+
+
+
+
